bd3916ddaa1cc136f30892dbc493410975a10bba
Manu <emmanuel.lamon@gmail.com>
2021-11-12 22:34:10 +0100

    add images and flipcard function

--- MemoryGame/script/start.js
+++ MemoryGame/script/start.js
@@ -4,63 +4,63 @@ function startgame() {
     const cardarray = [
         
         {
-            name: 'fries',
-            img: 'images/fries.png'
+            name: 'blue',
+            img: 'images/blue.png'
             
         },
         {
-            name: 'fries',
-            img: 'images/fries.png'
+            name: 'blue',
+            img: 'images/blue.png'
             
         },
         {
-            name: 'fries',
-            img: 'images/fries.png'
+            name: 'brown',
+            img: 'images/brown.png'
             
         },
         {
-            name: 'fries',
-            img: 'images/fries.png'
+            name: 'brown',
+            img: 'images/brown.png'
             
         },
         {
-            name: 'fries',
-            img: 'images/fries.png'
+            name: 'colors',
+            img: 'images/colors.png'
             
         },
         {
-            name: 'fries',
-            img: 'images/fries.png'
+            name: 'colors',
+            img: 'images/colors.png'
             
         },
         {
-            name: 'fries',
-            img: 'images/fries.png'
+            name: 'gray',
+            img: 'images/gray.png'
             
         },
         {
-            name: 'fries',
-            img: 'images/fries.png'
+            name: 'gray',
+            img: 'images/gray.png'
             
         },
         {
-            name: 'fries',
-            img: 'images/fries.png'
+            name: 'green',
+            img: 'images/green.png'
             
         },
         {
-            name: 'fries',
-            img: 'images/fries.png'
+            name: 'green',
+            img: 'images/green.png'
             
         },
         {
-            name: 'fries',
-            img: 'images/fries.png'
+            name: 'stripes',
+            img: 'images/stripes.png'
             
         },
         {
-            name: 'fries',
-            img: 'images/fries.png'
+            name: 'stripes',
+            img: 'images/stripes.png'
             
         }
     ]
@@ -68,6 +68,9 @@ function startgame() {
 
     const gridbox = document.querySelector(".gridbox");
     createboard();
+
+    var cardChosen = [];
+    var cardChosenId = [];
 
     function createboard() {
 
@@ -77,9 +80,42 @@ function startgame() {
             image.setAttribute('src', 'images/blank.png');
             image.setAttribute('width', '100px');
             image.setAttribute('height', '100px');
+            image.setAttribute('data-id', i);
+            image.addEventListener('click', flipcard);
             gridbox.appendChild(image);
         }
    
+    }
+
+    function checkForMatch() {
+
+        var cards = document.querySelectorAll('img');
+        const optionOneId = cardChosenId[0];
+        const optionTwoId = cardChosenId[1];
+        if (cardChosen[0] === cardChosen[1]) {
+            alert('you found a match');
+            cards[optionOneId].setAttribute('src', 'images/white.png');
+            cards[optionTwoId].setAttribute('src', 'images/white.png');
+        } else {
+
+        cards[optionOneId].setAttribute('src', 'images/blank.png');
+        cards[optionTwoId].setAttribute('src', 'images/blank.png');
+        }
+
+        cardChosenId = [];
+        cardChosen = [];
+    }
+
+    function flipcard() {
+
+        var imageId = this.getAttribute('data-id');
+        cardChosen.push(cardarray[imageId].name);
+        cardChosenId.push(imageId);
+        this.setAttribute('src', cardarray[imageId].img);
+
+        if (cardChosenId.length === 2) {
+            setTimeout(checkForMatch, 500);
+        }
     }
 
 }
